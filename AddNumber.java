5f090272093b1e5a5031ae4e8d07d06cec416879
DeepakBajare <deepakbajare@9998>
2021-09-14 11:16:38 +0530

    my file

---
+++ AddNumber.java
@@ -0,0 +1,16 @@
+class AddNumber{
+	public static void main(String args[]){
+	
+	add(1,8);
+	add(1,8,9);
+  
+}
+	public static void add(int a, int b){
+	System.out.println(a+b);
+
+}
+	public static void add(int a, int b, int c){
+	System.out.println(a+b+c);
+
+}
+}
---
+++ Multiplication.java
@@ -0,0 +1,17 @@
+class Multiplication{
+			
+public static void main(String args[]){
+
+	multiple(7,8);
+	multiple(7,8,9);
+}
+
+public static void multiple(int a,int b){
+System.out.println(a*b);
+}
+
+public static void multiple(int a, int b, int c){
+System.out.println(a*b*c);
+}
+	
+}
